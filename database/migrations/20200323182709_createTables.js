a1fc2d2eefac78482f8fb42d04f6987220232996
Mike Sinnema <msinnema33@gmail.com>
2020-03-23 18:32:54 -0700

    added dbconfig and ran createTable

--- database/migrations/20200323182709_createTables.js
+++ database/migrations/20200323182709_createTables.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+      users.increments();
   
-};
-
-exports.down = function(knex) {
+      users
+        .string('username', 128)
+        .notNullable()
+        .unique();
+      users.string('password', 128).notNullable();
+      users.string('department', 128).notNullable();
+    });
+  };
   
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  
