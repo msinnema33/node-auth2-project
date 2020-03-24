1d35a19d17660e92d1d3a4951b2f2b96f2b5a55c
Mike Sinnema <msinnema33@gmail.com>
2020-03-24 10:23:42 -0700

    updated mw w/ jwtSecret

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets');
+const { jwtSecret } = require('../config/secrets');
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
 
     if(authorization) {
 
-      jwt.verify(authorization, secrets.jwtSecret, function(err, decodedToken) {
+      jwt.verify(authorization, jwtSecret, function(err, decodedToken) {
         if(err) {
           res.status(401).json({ message: "You are not authorized to perform this operation"});
         } else {
